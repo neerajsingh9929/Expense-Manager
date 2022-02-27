8f7e66fe90909af842a3addf0f0755a6fd0e42b1
Neeraj Singh <100077922+neerajsingh9929@users.noreply.github.com>
2022-02-27 21:59:28 +0530

    Add files via upload

---
+++ help.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color:rebeccapurple ;
+        }
+    </style>
+</head>
+<body>
+    <h1 align="center">Click here to mail us </h1>
+    <div align="center"style="margin-top:100px;"><a
+        href="mailto:ns341920@gmail.com"><img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXP5K01x0_rkoT1L7uz9BV6d8bVOkMmY1ShQ&usqp=CAU" alt="rich dance project" width="175"
+          height="175" border="2"></a><br>
+    </div>
+    
+</body>
+</html>
