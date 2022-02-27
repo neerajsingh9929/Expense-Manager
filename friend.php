95a9038cb756217b2bd64db2b68885d76891ee43
Neeraj Singh <100077922+neerajsingh9929@users.noreply.github.com>
2022-02-27 21:58:44 +0530

    Add files via upload

---
+++ friend.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>friend</title>
+</head>
+<body>
+    <center><div class="a" style="background-color: seagreen;width: 50%;">
+    
+    <select >
+        <form action=""method="POST">
+            Name <input type="text"name="friendname"style="margin-top:10%"><br>
+            
+            <input type="button"value="Add"style="margin-top:2%">
+        </form>
+    </select>
+
+</div>
+</center>
+</body>
+</html>
